0216d28bf491323a2e4920a27f99a04cde42cb2b
Amal Zakirov <116391159+azaiv@users.noreply.github.com>
2025-10-24 17:08:16 +0300

    композиция

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
+	"net/url"
+	"time"
 )
 
 type account struct {
 	login    string
 	password string
 	url      string
+}
+
+type accountWithTimeStamp struct {
+	createdAt time.Time
+	updatedAt time.Time
+	account
 }
 
 func (acc *account) outputPassword() {
@@ -23,23 +32,68 @@ func (acc *account) generatePassword(n int) {
 	acc.password = string(res)
 }
 
+func newAccount(login, password, urlString string) (*accountWithTimeStamp, error) {
+	if login == "" {
+		return nil, errors.New("login required")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("Invalid URL")
+	}
+
+	newAcc := &accountWithTimeStamp{
+		account:   account{login: login, password: password, url: urlString},
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+	}
+
+	if password == "" {
+		newAcc.generatePassword(12)
+	}
+
+	return newAcc, nil
+}
+
+func newAccount(login, password, urlString string) (*account, error) {
+	if login == "" {
+		return nil, errors.New("login required")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("Invalid URL")
+	}
+
+	newAcc := &account{
+		login,
+		password,
+		urlString,
+	}
+
+	if password == "" {
+		newAcc.generatePassword(12)
+	}
+
+	return newAcc, nil
+}
+
 func main() {
 	login := promtData("Введите логин:")
+	password := promtData("Введите пароль:")
 	url := promtData("Введите URL:")
 
-	myAccount := account{
-		login: login,
-		url:   url,
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	myAccount.generatePassword(12)
 	myAccount.outputPassword()
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLM")
 
 func promtData(promt string) string {
-	fmt.Println(promt)
+	fmt.Print(promt)
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
 }
